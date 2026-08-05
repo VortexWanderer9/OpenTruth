7cb665d83b215347c68357a266914809e8ceb664
Tyrell Wellick <keshavniure99@gmail.com>
2026-08-05 07:32:14 -0800

    refactor: update Web3.Storage import paths for consistency and type declaration

--- lib/web3/ipfs.ts
+++ lib/web3/ipfs.ts
@@ -1,4 +1,4 @@
-import { Web3Storage } from 'web3.storage/dist/src/lib.js'
+import { Web3Storage } from 'web3.storage'
 import { IPFS_GATEWAYS } from './config'
 
 // Initialize Web3.Storage client
---
+++ types/web3-storage.d.ts
@@ -0,0 +1,3 @@
+declare module 'web3.storage' {
+  export * from 'web3.storage/dist/src/lib.js'
+}
